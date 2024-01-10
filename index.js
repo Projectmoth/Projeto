e1c8b60fc5c85dd152ae13f863b1df937b8c9026
Projectmoth <151297755+Projectmoth@users.noreply.github.com>
2024-01-10 13:33:06 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ function checkPassword() {
     var errorMessage = document.getElementById("errorMessage");
 
     // Substitua "suaSenha" pela senha desejada
-    var senhaCorreta = "c0m3t0u5";
+    var senhaCorreta = "04012024";
     if (passwordInput === senhaCorreta) {
         // Redirecionar para scratch
         window.location.href = "https://drive.google.com/file/d/1TF3mxntdMeWaoafySZFfIVxBzMyivfvz/view?usp=drivesdk";
