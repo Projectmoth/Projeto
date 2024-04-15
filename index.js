5b7c6e3a90f9c700a693a50cfa84c352274bbdb3
Projectmoth <151297755+Projectmoth@users.noreply.github.com>
2024-04-15 10:46:43 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function checkPassword() {
     var senhaCorreta = "orangetrump";
     if (passwordInput === senhaCorreta) {
         // Redirecionar para scratch
-        window.location.href = " https://youtu.be/xvFZjo5PgG0?si=F0tfx8D1hQCbVwzT ";
+        window.location.href = "https://youtu.be/xvFZjo5PgG0?si=F0tfx8D1hQCbVwzT";
     } else {
         errorMessage.textContent = "Senha incorreta. Tente novamente.";
     }
